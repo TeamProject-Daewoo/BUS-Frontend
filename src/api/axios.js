950a855b45318e4e1f5caf6e784e483b4f1a322c
Yongseok-2 <8465867@naver.com>
2025-09-20 16:20:11 +0900

    fix: 시큐리티 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/api/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8889',
+  baseURL: import.meta.env.VITE_BACK_ADMIN_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true, // refreshToken 쿠키 전송
 })
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import RoomsView from '@/views/RoomsView.vue'
 import SettingsView from '@/views/SettingsView.vue'           // 개별 수정
 import HotelsSelectView from '@/views/HotelsSelectView.vue'   // 목록
 import HotelCreateView from '@/views/HotelCreateView.vue'
+import { watch } from 'vue'
 
 import { useAuthStore } from '@/api/auth'
 import { useHotelStore } from '@/stores/hotel'
@@ -41,14 +42,24 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const auth = useAuthStore()
+  const authStore = useAuthStore()
   const hotel = useHotelStore()
+  const isLoggedIn = !!authStore.accessToken
 
-  if (!auth.isInitialized) auth.init()
+    if (authStore.isInitialized) {
+    handleNavigation(to, from, next)
+  } else {
+    const unwatch = watch(() => authStore.isInitialized, (isInitialized) => {
+      if (isInitialized) {
+        handleNavigation(to, from, next)
+        unwatch()
+      }
+    })
+  }
   if (!hotel.hotels?.length && !hotel.selectedContentId) hotel.restore()
 
-  if (to.meta.requiresAuth && !auth.accessToken) {
-    return next({ name: 'login', query: { redirect: to.fullPath } })
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return next({ name: '/', query: { redirect: to.fullPath } })
   }
   if (to.meta.requiresHotel && !hotel.selectedContentId) {
     // 호텔 선택 필요한 화면(예약/객실 등)은 목록으로 보냄
@@ -56,5 +67,25 @@ router.beforeEach((to, from, next) => {
   }
   next()
 })
+
+function handleNavigation(to, from, next) {
+  const authStore = useAuthStore()
+  const isLoggedIn = !!authStore.accessToken
+
+  // 이동하려는 페이지가 로그인이 필요한데, 로그인하지 않은 경우
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    alert('로그인이 필요한 서비스입니다.')
+    next('/') // 👈 요청하신 대로 메인 페이지('/')로 이동
+  }
+  // 이동하려는 페이지가 로그아웃 상태여야 하는데, 로그인한 경우
+  else if (to.meta.requiresGuest && isLoggedIn) {
+    // 이미 로그인했다면, 대시보드 페이지로 보냅니다.
+    next('/dashboard') 
+  } 
+  // 그 외의 경우는 정상적으로 통과
+  else {
+    next()
+  }
+}
 
 export default router
